b58d7462384adbbacca994477cbe64ecc69ffba0
Ipekduru <ipekduru33830@gmail.com>
2025-11-19 10:25:36 +0300

    Fix development appsettings and configuration

--- Core/HepsiSln.Domain/obj/Debug/net7.0/HepsiSln.Domain.AssemblyInfo.cs
+++ Core/HepsiSln.Domain/obj/Debug/net7.0/HepsiSln.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HepsiSln.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427cd489b3785743de69721c70001a5fc3d6e46e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1335fd63f75cb48f185dc52e8fbc41708450e90a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HepsiSln.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HepsiSln.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Presantation/HepsiSln.Api/Program.cs
+++ Presantation/HepsiSln.Api/Program.cs
@@ -7,6 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var env= builder.Environment;
+//hepsiSln.Api nin pathi
+builder.Configuration.SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false) // appsettingsi her zamn bulur 
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);// development a ozel appsetting
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
